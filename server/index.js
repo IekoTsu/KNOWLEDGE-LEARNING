2aa0db4eb0b8ab1c4ea1f9822abac10f9f57afe9
IekoTsu <bahamilouah@gmail.com>
2024-12-03 14:29:21 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -97,7 +97,7 @@ app.use(helmet());
  */
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 200 // limit each IP to 100 requests per windowMs
+  max: 200 // limit each IP to 200 requests per windowMs
 });
 app.use(limiter);
 
